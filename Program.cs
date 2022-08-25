ab2c563e719de52f93625c9e41ba65a7296c1ba7
Хаджимурад <oo05oo@bk.ru>
2022-08-25 19:13:57 +0300

    Добавил все 3 задачи

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите число ");
+int A = int.Parse(Console.ReadLine());
+Console.Write($"Вторая цифра {(( A % 100 )-( A % 10 )) / 10 }");
+
+Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.*/
+
+Console.WriteLine("Введите число ");
+int A = int.Parse(Console.ReadLine());
+if ( A > 99 )
+{
+Console.Write($"Третья цифра = {((A % 1000)-(A % 100))/100}");
+}
+else
+{
+    Console.Write(" нету третьей цифры");
+}
+
+/*Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+bool A ( int n ) { return n==1|n==2|n==3|n==4|n==5; }
+bool flag = A ( 1 );
+Console.Write( A ( 4 ) );*/
