641bc40a4c905dbd3b08f2866ad6fee2338f3bad
Хаджимурад <oo05oo@bk.ru>
2022-09-19 20:03:20 +0300

    Добавил задачи 3 8-го  урокa

--- Homework8/Program.cs
+++ Homework8/Program.cs
@@ -8,50 +8,6 @@
 // 9 5 3 2
 // 8 4 4 2
 
-Console.WriteLine("Введите количество строк: ");
-int A = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите количество столбцов: ");
-int B = Convert.ToInt32(Console.ReadLine());
-int[,] array = GetArray (A, B, 0, 100);
-PrintArray(array);
-int[,] GetArray(int m, int n, int min, int max)
-{
-    int[,] result = new int[m, n];
-      for (int i = 0; i < m; i++)
-      {
-        for (int j = 0; j < n; j++)
-        {
-          result[i, j] = new Random().Next(min, max +1);
-        }
-      }
-      return result;
-}
-void PrintArray(int[,] array)
-{
-      for (int i = 0; i < array.GetLength(0); i++)
-      {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-          Console.Write($"{array[i, j]} ");
-        }
-        Console.WriteLine();
-      }
-}
-
-
-
-
-
-
-// Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
-// Например, задан массив:
-// 1 4 7 2
-// 5 9 2 3
-// 8 4 2 4
-// 5 2 6 7
-// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
-
-
 // Console.WriteLine("Введите количество строк: ");
 // int A = Convert.ToInt32(Console.ReadLine());
 // Console.WriteLine("Введите количество столбцов: ");
@@ -85,6 +41,83 @@ void PrintArray(int[,] array)
 
 
 
+
+
+// Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+
+// int m = InputNumbers("Введите m: ");
+// int n = InputNumbers("Введите n: ");
+// int range = InputNumbers("Введите диапазон: от 1 до ");
+
+// int[,] array = new int[m, n];
+// CreateArray(array);
+// WriteArray(array);
+
+// int minSumLine = 0;
+// int sumLine = SumLineElements(array, 0);
+// for (int i = 1; i < array.GetLength(0); i++)
+// {
+//   int tempSumLine = SumLineElements(array, i);
+//   if (sumLine > tempSumLine)
+//   {
+//     sumLine = tempSumLine;
+//     minSumLine = i;
+//   }
+// }
+
+// Console.WriteLine($"\n{minSumLine+1} - строкa с наименьшей суммой ({sumLine}) элементов ");
+
+
+// int SumLineElements(int[,] array, int i)
+// {
+//   int sumLine = array[i,0];
+//   for (int j = 1; j < array.GetLength(1); j++)
+//   {
+//     sumLine += array[i,j];
+//   }
+//   return sumLine;
+// }
+
+// int InputNumbers(string input)
+// {
+//   Console.Write(input);
+//   int output = Convert.ToInt32(Console.ReadLine());
+//   return output;
+// }
+
+// void CreateArray(int[,] array)
+// {
+//   for (int i = 0; i < array.GetLength(0); i++)
+//   {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//       array[i, j] = new Random().Next(range);
+//     }
+//   }
+// }
+
+// void WriteArray (int[,] array)
+// {
+//   for (int i = 0; i < array.GetLength(0); i++)
+//   {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//       Console.Write(array[i,j] + " ");
+//     }
+//     Console.WriteLine();
+//   }
+// }
+
+
+
+
 // Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
 // Например, даны 2 матрицы:
 // 2 4 | 3 4
@@ -94,8 +127,72 @@ void PrintArray(int[,] array)
 // 15 18
 
 
+// int m = InputNumbers("Введите число строк 1-й матрицы: ");
+// int n = InputNumbers("Введите число столбцов 1-й матрицы (и строк 2-й): ");
+// int p = InputNumbers("Введите число столбцов 2-й матрицы: ");
+// int range = InputNumbers("Введите диапазон случайных чисел: от 1 до ");
 
+// int[,] firstMartrix = new int[m, n];
+// CreateArray(firstMartrix);
+// Console.WriteLine($"\nПервая матрица:");
+// WriteArray(firstMartrix);
 
+// int[,] secomdMartrix = new int[n, p];
+// CreateArray(secomdMartrix);
+// Console.WriteLine($"\nВторая матрица:");
+// WriteArray(secomdMartrix);
+
+// int[,] resultMatrix = new int[m,p];
+
+// MultiplyMatrix(firstMartrix, secomdMartrix, resultMatrix);
+// Console.WriteLine($"\nПроизведение первой и второй матриц:");
+// WriteArray(resultMatrix);
+
+// void MultiplyMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix)
+// {
+//   for (int i = 0; i < resultMatrix.GetLength(0); i++)
+//   {
+//     for (int j = 0; j < resultMatrix.GetLength(1); j++)
+//     {
+//       int sum = 0;
+//       for (int k = 0; k < firstMartrix.GetLength(1); k++)
+//       {
+//         sum += firstMartrix[i,k] * secomdMartrix[k,j];
+//       }
+//       resultMatrix[i,j] = sum;
+//     }
+//   }
+// }
+
+// int InputNumbers(string input)
+// {
+//   Console.Write(input);
+//   int output = Convert.ToInt32(Console.ReadLine());
+//   return output;
+// }
+
+// void CreateArray(int[,] array)
+// {
+//   for (int i = 0; i < array.GetLength(0); i++)
+//   {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//       array[i, j] = new Random().Next(range);
+//     }
+//   }
+// }
+
+// void WriteArray (int[,] array)
+// {
+//   for (int i = 0; i < array.GetLength(0); i++)
+//   {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//       Console.Write(array[i,j] + " ");
+//     }
+//     Console.WriteLine();
+//   }
+// }
 
 
 
@@ -107,7 +204,62 @@ void PrintArray(int[,] array)
 // 26(1,0,1) 55(1,1,1)
 
 
+// Console.WriteLine("Введите размеры массива через пробел: ");
+// string[] nums = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+// int[,,] array = GetArray(new int[] { int.Parse(nums[0]), int.Parse(nums[1]), int.Parse(nums[2]), }, 10, 99);
+// PrintArray(array);
 
+// int[,,] GetArray(int[] sizes, int min, int max)
+// {
+//     int[,,] result = new int[sizes[0], sizes[1], sizes[2]];
+//     for (int i = 0; i < result.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < result.GetLength(1); j++)
+//         {
+//             int k = 0;
+//             while (k < result.GetLength(2))
+//             {
+//                 int element = new Random().Next(min, max + 1);
+//                 if (FindElement(result, element)) continue;
+//                 result[i, j, k] = element;
+//                 k++;
+//             }
+//         }
+//     }
+//     return result;
+// }
+
+
+//         bool FindElement(int[,,] array, int el)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             for (int k = 0; k < array.GetLength(2); k++)
+//             {
+//                 if (array[i, j, k] == el) return true;
+//             }
+//         }
+//     }
+//     return false;
+// }
+// void PrintArray(int[,,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             for (int k = 0; k < array.GetLength(2); k++)
+//             {
+//                 Console.Write($"{array[i, j, k]} ({i},{j},{k}) ");
+//             }
+//             Console.WriteLine();
+
+//             Console.ReadKey();
+//         }
+//     }
+// }
 
 
 // Напишите программу, которая заполнит спирально массив 4 на 4.
